bd31cc0a713c9e4dcbac5348dc26bc20d95b9378
Hophouse <contact@qro.me>
2020-09-22 23:26:02 +0200

    Update serve command to add Web authentication. Basic is working. NTLM needs to parsed.

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,26 +1,26 @@
 package cmd
 
 import (
-
-    "github.com/hophouse/gop/gopServe"
-    "github.com/hophouse/gop/utils"
+	gopserve "github.com/hophouse/gop/gopServe"
+	"github.com/hophouse/gop/utils"
 	"github.com/spf13/cobra"
 )
 
 var (
 	directoryServeOption string
+	authOption           string
 )
 
 // serveCmd represents the serve command
 var serveCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "Serve a specific directory through an HTTP server.",
-	Long: "Serve a specific directory through an HTTP server.",
+	Long:  "Serve a specific directory through an HTTP server.",
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		utils.NewLoggerStdout()
 	},
 	Run: func(cmd *cobra.Command, args []string) {
-		gopserve.RunServeCmd(hostOption, portOption, directoryServeOption)
+		gopserve.RunServeCmd(hostOption, portOption, directoryServeOption, authOption)
 	},
 }
 
@@ -30,4 +30,5 @@ func init() {
 	serveCmd.PersistentFlags().StringVarP(&hostOption, "Host", "H", "127.0.0.1", "Define the proxy host.")
 	serveCmd.PersistentFlags().StringVarP(&portOption, "Port", "P", "8000", "Define the proxy port.")
 	serveCmd.PersistentFlags().StringVarP(&directoryServeOption, "directory", "d", ".", "Directory to serve.")
+	serveCmd.PersistentFlags().StringVarP(&authOption, "auth", "a", "", "Add an authentication option to the server. Could be either \"Basic\" or \"NTLM\".")
 }
--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gocolly/colly v1.2.0
 	github.com/gookit/color v1.2.4
+	github.com/gorilla/mux v1.8.0
 	github.com/jroimartin/gocui v0.4.0
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/mattn/go-runewidth v0.0.8 // indirect
@@ -20,6 +21,7 @@ require (
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/spf13/cobra v1.0.0
 	github.com/temoto/robotstxt v1.1.1 // indirect
+	github.com/urfave/negroni v1.0.0
 	github.com/vbauerster/mpb/v5 v5.0.3
 	golang.org/x/text v0.3.2 // indirect
 )
