2d609eaaa01dfdd60156595f8edb731a86d8b306
Hophouse <contact@qro.me>
2020-11-18 12:39:05 +0100

    Minor modification to put the worker in a dedicated function.

--- gopHost/gophost.go
+++ gopHost/gophost.go
@@ -6,7 +6,6 @@ import (
 	"net"
 	"os"
 	"strings"
-	"sync"
 	"text/tabwriter"
 )
 
@@ -17,32 +16,25 @@ type register struct {
 
 // RunHostCmd : Run the Host command and do a DNS lookup for each host inputed
 func RunHostCmd(reader *os.File, concurrency int) {
-	var mutex sync.RWMutex
-
 	// Scanner to read file
 	scanner := bufio.NewScanner(reader)
 	workersChan := make(chan bool)
 	domainsChan := make(chan string, concurrency)
+	gatherChan := make(chan register)
 	results := make([]register, 0)
 
 	for i := 0; i < concurrency; i++ {
-		go func() {
-			for domain := range domainsChan {
-				ipList, err := net.LookupHost(domain)
-				if err != nil {
-					fmt.Printf("Error for domain %s : %s", domain, err)
-				}
-				mutex.Lock()
-				results = append(results, register{
-					domain: domain,
-					ip:     ipList,
-				})
-				mutex.Unlock()
-			}
-			workersChan <- true
-		}()
+		go worker(domainsChan, gatherChan, workersChan)
 	}
 	defer close(workersChan)
+
+	// Consumme the results from the worker
+	go func() {
+		for elem := range gatherChan {
+			results = append(results, elem)
+		}
+		workersChan <- true
+	}()
 
 	// Read domains, add them to the channel, then close it
 	for scanner.Scan() {
@@ -55,11 +47,27 @@ func RunHostCmd(reader *os.File, concurrency int) {
 		<-workersChan
 	}
 
-	// Consumme results
-	w := tabwriter.NewWriter(os.Stdout, 8, 8, 0, '\t', 0)
+	// Close the channel and then wait for the gather worker to finish
+	close(gatherChan)
+	// Last worker is the one that consumme workers' results and adf them to the results slice
+	<-workersChan
+
+	// Display results
+	w := tabwriter.NewWriter(os.Stdout, 8, 4, 2, ' ', 0)
 	for _, elem := range results {
 		ipList := strings.Join(elem.ip, " ")
 		fmt.Fprintf(w, "%s\t%s\n", elem.domain, ipList)
 	}
 	w.Flush()
+}
+
+func worker(domainsChan chan string, gatherChan chan register, workersChan chan bool) {
+	for domain := range domainsChan {
+		ipList, _ := net.LookupHost(domain)
+		gatherChan <- register{
+			domain: domain,
+			ip:     ipList,
+		}
+	}
+	workersChan <- true
 }
